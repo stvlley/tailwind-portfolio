6f3de3b303e94e7c051182c055682d0e6cf17aca
Stephen <stvlley.amzn@gmail.com>
2022-05-19 18:30:30 -0400

    finished medium api fetch

--- pages/Published.js
+++ pages/Published.js
@@ -1,89 +1,98 @@
 import React from 'react'
 import Image from 'next/image'
+import axios from "axios";
+import { useState, useEffect } from 'react';
+
 
 export default function Published() {
 
-    const mediumToken = "29927c09263f063383c3f02469796edf9ecc4455a2e68e1fd538575c5897da2e5"
+    const [blogs, setBlogs] = useState([])
 
-   
+    
 
+    useEffect(() => {
+        fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@stephentalleyjr')
+            .then(resp => resp.json())
+            .then(data => {
+                setBlogs(data.items)
+            })
+            
+    }, [])
+    const image = blogs.map(blog =>  <img src={blog.thumbnail} width={300} height={300}/>)
 
+    
     return (
         <div name='projects' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full '>
-                <div>
-                    <p className='text-4xl font-bold inline border-b-4 border-green-400 mb-10'>Published</p>
-                    <p className='py-6'>Some of my latest blog posts</p>
-                </div>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full '>
+          <div>
+              <p className='text-4xl font-bold inline border-b-4 border-green-400 mb-10'>Published </p>
+              <p className='py-6'>Some of my latest blog posts</p>
+          </div>
+      
 
-
-                <div className='w-full  grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
-
-                    <div className='group shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <Image src="/nextjs-dark.svg" alt="HTML Logo" width={300} height={300} />
-                        <p className='my-4'>NextJS / MongoDB </p>
-
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                Digital Fashion House
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href='/'>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                </a>
-                                <a href='/'>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                </a>
-                            </div>
-
+          <div className='w-full  grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
+             
+              <div className='group shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+              {image[1]}
+              {/* <Image src="/nextjs-dark.svg" alt="HTML Logo" width={300} height={300} /> */}
+                  <p className='my-4 text-2xl'>{blogs[1].title}</p>
+                  <p>By Stephen Talley</p>
+                  <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-1xl font-bold text-green-400 tracking-wider'>
+                       Comparing the two Ruby frameworks
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>View Article</button>
+                            </a>
+                            
                         </div>
+
                     </div>
-                    <div className='group shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <Image src="/nextjs-dark.svg" alt="HTML Logo" width={300} height={300} />
-                        <p className='my-4'>HTML</p>
-
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                Console.S()uad
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href='/'>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                </a>
-                                <a href='/'>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                </a>
-                            </div>
-
+              </div>
+              <div className='group shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+              {image[0]}
+              {/* <Image src="/nextjs-dark.svg" alt="HTML Logo" width={300} height={300} /> */}
+                  <p className='my-4 text-2xl'>{blogs[0].title}</p>
+                  <p>By Stephen Talley</p>
+                  <div className='opacity-0 group-hover:opacity-100'>
+                  <span className='text-1xl font-bold text-green-400 tracking-wider'>
+                       Step-By-Step Guide on how to use the Google Maps API
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>View Article</button>
+                            </a>
+                            
                         </div>
+
                     </div>
-                    <div className='group shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <Image src="/nextjs-dark.svg" alt="HTML Logo" width={300} height={300} />
-                        <p className='my-4'>HTML</p>
-
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                East Coast Fishing Co.
-                            </span>
-                            <p>Team project</p>
-                            <div className='pt-8 text-center'>
-                                <a href='/'>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                </a>
-                                <a href='/'>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                </a>
-                            </div>
-
+              </div>
+              <div className='group shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+              {image[5]}
+              {/* <Image src="/nextjs-dark.svg" alt="HTML Logo" width={300} height={300} /> */}
+                  <p className='my-4 text-2xl'>{blogs[5].title}</p>
+                <p>By Stephen Talley</p>
+                  <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-1xl font-bold text-green-400 tracking-wider'>
+                       A novices' thought process on Object-Oriented Design
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>View Article</button>
+                            </a>
+                            
                         </div>
+
                     </div>
-
-
-
-                </div>
-
-            </div>
-        </div>
+              </div>
+           
+            
+              
+          </div>
+  
+      </div>
+    </div>
     )
 }
 
